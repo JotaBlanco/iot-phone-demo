b80594bd19353920997ebd99ad411c58293a0483
Javier Blanco Cordero <javier@quix.io>
2025-01-21 15:16:16 +0000

    Edited 'main.py'

--- Debugging Transformation/main.py
+++ Debugging Transformation/main.py
@@ -16,7 +16,7 @@ sdf = app.dataframe(input_topic)
 # see docs for what you can do
 # https://quix.io/docs/get-started/quixtour/process-threshold.html
 
-sdf.print()
+sdf.print(schema=True)
 sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
