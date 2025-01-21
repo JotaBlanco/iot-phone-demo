c619fd715330b7465d64d2bd75f4b07cf1e494f3
Javier Blanco Cordero <javier@quix.io>
2025-01-21 17:33:22 +0000

    Edited 'main.py'

--- Debugging Transformation/main.py
+++ Debugging Transformation/main.py
@@ -11,13 +11,23 @@ input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
 
 sdf = app.dataframe(input_topic)
-filter_cols = (
-    (sdf.contains("location-altitude")) & 
-    (sdf.contains("location-longitude")) &
-    (sdf.contains("location-latitude"))
-    )
+
+"""
+filter_cols = ((sdf.contains("location-altitude")) & (sdf.contains("location-longitude")) & (sdf.contains("location-latitude")))
 sdf = sdf[filter_cols]
 sdf = sdf[["location-altitude", "location-longitude", "location-latitude"]]
+
+cols = ["location-altitude", "location-longitude", "location-latitude"]
+def check_if_cols_exist(values):
+    return all([col in values for col in cols])
+"""
+
+if sdf.update(check_if_cols_exist):
+    sdf = sdf[["location-altitude", "location-longitude", "location-latitude"]]
+else:
+    for col in cols:
+        sdf[col] = None
+    sdf = sdf[["location-altitude", "location-longitude", "location-latitude"]]
 
 
 sdf.print(metadata=True)
@@ -25,3 +35,5 @@ sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run()
+
+
