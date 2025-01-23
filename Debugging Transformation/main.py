0478d13c38cc10e58d1872b6b7e2c8959768efcf
Tomas N <tomas+javi@quix.io>
2025-01-23 14:15:52 +0000

    Edited 'main.py'

--- Debugging Transformation/main.py
+++ Debugging Transformation/main.py
@@ -10,7 +10,7 @@ import numpy as np
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
+app = Application(consumer_group="transformation-v1.1", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
